cdc14a1eef1fcb2e6efeb32b1b758361e1827f11
Renato Santos <renato@midilabs.com.br>
2022-06-08 11:52:02 -0300

    Antes de update do angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
+import { listenerCount } from 'process';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/button', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  public menuCollection = [
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Button', url: '/button', icon: 'dice' },
+    { title: 'Card', url: '/card', icon: 'qr-code' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() { }
 }
